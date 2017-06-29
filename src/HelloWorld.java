db82c4fa858321b89d135b44e9e0183557bfac57
Piotr Lesiecki <lesieck.pio@ad.meelogic.com>
2017-06-29 14:44:38 +0200

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lesieck.pio on 29.06.2017.
+ */
+public class HelloWorld {
+    public static final void main(String[] cmd_lineParams)  {
+        System.out.println("Hello World!");
+    }
+}
